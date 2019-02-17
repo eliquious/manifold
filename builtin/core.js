3ae81f0259f09086bf2c99c9759793f69a6f90a4
Max Franks <eliquious@gmail.com>
2019-02-17 15:22:16 -0600

    Add comments to core.js

--- builtin/core.js
+++ builtin/core.js
@@ -1,4 +1,5 @@
 
+// str2ab converts a string to an arraybuffer
 const str2ab = function(str) {
 	var buf = new ArrayBuffer(str.length);
 	var bufView = new Uint8Array(buf);
@@ -8,10 +9,12 @@ const str2ab = function(str) {
 	return buf;
 }
 
+// ab2str converts an arraybuffer to a string
 const ab2str = function(buf) {
 	return String.fromCharCode.apply(null, new Uint8Array(buf));
 };
 
+// Sends a msg to the API. Args needs to be an array of strings.
 export function send(fn, args) {
 	let resp = global.send(str2ab(JSON.stringify({
 		fn,
@@ -22,35 +25,3 @@ export function send(fn, args) {
 	if (val.err) throw (val.err);
 	return val.data;
 };
-
-// (function (root, factory) {
-//     if (typeof define === 'function' && define.amd) {
-//         // AMD. Register as an anonymous module.
-//         define([], function() {factory(root);});
-//     } else factory(root);
-// })(typeof exports !== "undefined" ? exports : this, function(root) {
-
-// 	const str2ab = function(str) {
-// 		var buf = new ArrayBuffer(str.length);
-// 		var bufView = new Uint8Array(buf);
-// 		for (var i = 0, strLen = str.length; i < strLen; i++) {
-// 			bufView[i] = str.charCodeAt(i);
-// 		}
-// 		return buf;
-// 	}
-
-// 	const ab2str = function(buf) {
-// 		return String.fromCharCode.apply(null, new Uint8Array(buf));
-// 	};
-
-// 	root.send = function(fn, args) {
-// 		let resp = global.send(str2ab(JSON.stringify({
-// 			fn,
-// 			args
-// 		})));
-
-// 		let val = JSON.parse(ab2str(resp));
-// 		if (val.err) throw (val.err);
-// 		return val.data;
-// 	};
-// });
